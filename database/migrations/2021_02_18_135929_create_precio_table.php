01387d5daf3b831222a3b0a258230e656ee0f190
Dulce Maria Sevilla Ortiz <dulcesevilla45@gmail.com>
2021-02-18 08:29:42 -0600

    migraciones

---
+++ database/migrations/2021_02_18_135929_create_precio_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrecioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('precio', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('imagen');
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('users_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('precio');
+    }
+}
---
+++ database/migrations/2021_02_18_142047_create_imagen_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imagen', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('users_id');
+            $table->unsignedBigInteger('cliente_id');
+            $table->unsignedBigInteger('venta_id');
+             $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imagen');
+    }
+}
